a7f8429ccd74186aec9b655c34fb2b3f7cf57f79
LucasGPestana <lucasgabrielpestana03@gmail.com>
2025-10-27 10:39:45 -0300

    feat: Implement plot_corr_barplot function

--- notebooks/src/graficos.py
+++ notebooks/src/graficos.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import ListedColormap, CenteredNorm
+
 import pandas as pd
 import seaborn as sns
 
 
-from typing import List
+from typing import List, Literal
+
 
 def pairplot(df: pd.DataFrame, columns: List[str], hue_column: str) -> None:
 
@@ -45,5 +49,50 @@ def plot_bivariate_boxplot(df: pd.DataFrame, columns: List[str], x_cat_column: s
             showmeans=True,
             ax=ax,
         )
+
+    plt.show()
+
+def plot_corr_barplot(df: pd.DataFrame, column: str, corr_method: Literal["pearson", "spearman"]="pearson") -> None:
+
+    """Plota um gráfico de barras da correlação entre 'column' e as demais colunas de 'df'
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Fonte dos dados
+    
+    column: str
+        Coluna a ser analisada a correlação
+    
+    corr_method: Literal["pearson", "spearman"]
+        Tipo de correlação. Pode ser de pearson (relação linear) ou spearman (relação monocotômica).
+
+    """
+
+    series_corr = df.corr(method=corr_method)[column].drop(column).sort_values()
+
+    cmap = "coolwarm_r"
+    cnorm = CenteredNorm(vcenter=0, halfrange=max([abs(series_corr.min()), abs(series_corr.max())]))
+    smap = ScalarMappable(norm=cnorm, cmap=cmap)
+
+    listed_colors = ListedColormap([smap.to_rgba(value) for value in series_corr.values]).colors
+
+
+    _, ax = plt.subplots(figsize=(16 ,6))
+
+    sns.barplot(
+        x=series_corr.index,
+        y=series_corr.values,
+        hue=series_corr.values,
+        legend=False,
+        palette=listed_colors,
+        ax=ax
+    )
+
+    ax.set_xlabel("Atributos")
+    ax.set_ylabel("Correlação")
+    ax.set_title("Correlação de Pearson entre Response e outros atributos")
+
+    ax.tick_params(axis='x', rotation=90)
 
     plt.show()
